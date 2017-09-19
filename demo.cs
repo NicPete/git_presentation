dedd60c07f21dcce79a323a1e5f629d719685974
Max <test@test.com>
2017-09-19 21:49:47 +0200

    initial commit

---
+++ demo.cs
@@ -0,0 +1,19 @@
+using System;
+namespace HelloWorld
+{
+    class Hello 
+    {
+        static void Main() 
+        {
+            HelloWorld();
+        }
+		
+		static void HelloWorld()
+		{
+			Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+		}
+		
+    }
+}
